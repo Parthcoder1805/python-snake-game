8a05f373a1cdf1ac01e56368580817877551627c
Parth Awasthi <parthawasthi2006@gmail.com>
2026-06-28 19:46:34 +0530

    Add persistent high score using file handling

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("high_score.txt") as file:
+            self.high_score = int(file.read())
+
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -14,12 +17,17 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.goto(0,260)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
 
     def increase_score(self):
             self.score += 1
-            self.clear()
+            if self.score > self.high_score:
+                self.high_score = self.score
+                with open("high_score.txt", mode = "w") as file:
+                    file.write(str(self.high_score))
             self.update_scoreboard()
 
     def start_message(self):
@@ -39,7 +47,6 @@ class Scoreboard(Turtle):
 
 
     def reset(self):
-        self.clear()
         self.score = 0
         self.goto(0,0)
         self.update_scoreboard()
